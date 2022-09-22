5ddd1956afd5b45b6566c295fad627bb1ce79fc8
Aleksandr Popov <pishite2011@yandex.ru>
2022-09-22 13:08:09 +0500

    составил программу по определению чётное число или нет

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// сравнить три введённых числа
+Console.WriteLine("Введите три любые целые числа");
+int user_nam_A = int.Parse(Console.ReadLine());
+int user_nam_B = int.Parse(Console.ReadLine());
+int user_nam_C = int.Parse(Console.ReadLine());
+int max = user_nam_A;
+
+if (user_nam_B > max)
+{
+    max = user_nam_B;
+}
+if (user_nam_C > max)
+{
+    max = user_nam_C;
+}
+Console.WriteLine("максимальное число " + max);
---
+++ task6/Program.cs
@@ -0,0 +1,12 @@
+﻿// программа, котрая определит чётное или нет введённое число
+Console.WriteLine("Введите любое целое число:");
+int user_nam = int.Parse(Console.ReadLine());
+
+if (user_nam % 2 == 0)
+{
+    Console.WriteLine("Это число чётное!");
+}
+else
+{
+    Console.WriteLine("Это число не чётное!");
+}
