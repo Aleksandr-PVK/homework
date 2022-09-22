a2f7ff448aa425876ac71757d6a578da6d9df4c1
Aleksandr Popov <pishite2011@yandex.ru>
2022-09-22 13:54:45 +0500

    закончил написание программы

---
+++ task8/Program.cs
@@ -0,0 +1,12 @@
+﻿//программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+Console.WriteLine("Введите любое целое число:");
+int user_nam = int.Parse(Console.ReadLine());
+int x = 1;
+    while (x < user_nam)
+{
+    if (x % 2 == 0)
+    {
+        Console.WriteLine(x);
+    }
+    int x = x + 1;
+}
