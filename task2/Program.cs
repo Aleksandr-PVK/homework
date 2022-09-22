31f4c9079551c26943d2da1ae41d7b06b78b6a45
Aleksandr Popov <pishite2011@yandex.ru>
2022-09-22 12:24:53 +0500

    создал программу сравнения чисел

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿//сравнение двух введённых чисел
+Console.WriteLine("Введите два любые целые числа:");
+int user_nam_A = int.Parse(Console.ReadLine());
+int user_nam_B = int.Parse(Console.ReadLine());
+
+if (user_nam_A > user_nam_B)
+{
+    Console.WriteLine("max = " + user_nam_A);
+    Console.WriteLine("min = " + user_nam_B);
+}
+else if(user_nam_A < user_nam_B)
+{
+    Console.WriteLine("max = " + user_nam_B);
+    Console.WriteLine("min = " + user_nam_A);
+}
+else 
+{
+    Console.WriteLine("Числа равны.");
+}
